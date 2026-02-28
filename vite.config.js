405f74470ae38607c7ba6d1500e5dc30f436367d
RK2KHub <raghulkannalakshmanan@gmail.com>
2026-02-28 02:42:00 -0400

    Resolve merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [react()],
   base: "/Raghul-wife_Birthday_App/",
-=======
-    plugins: [react()],
-    base: "/Raghul-wife_Birthday_App/",
->>>>>>> 24f0e457c930c1f15c2057b37169075106697bba
 });
